ef447578ff885c14c0971613f7ed2e36059ae64b
s3943811 <s3943811@student.rmit.edu.au>
2023-07-25 18:59:36 +1000

    Setup

---
+++ Insights/Insights/InsightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsightsApp.swift
+//  Insights
+//
+//  Created by Maximus Dionyssopoulos on 25/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct InsightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
